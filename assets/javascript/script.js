7fd4b8f9557998466753003a72c696f4a6e1e141
antieatingactivist <antieatingactivist@gmail.com>
2022-01-16 21:27:01 -0800

    implimented gradual scroll to top

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,13 @@
 // console.log("hello");
 containerEl = document.getElementById("container");
+project1 = document.getElementById("p1");
+project2 = document.getElementById("p2");
+project3 = document.getElementById("p3");
+project4 = document.getElementById("p4");
+project5 = document.getElementById("p5");
+project6 = document.getElementById("p6");
+project7 = document.getElementById("p7");
+
 
 
 
@@ -11,5 +19,31 @@ containerEl = document.getElementById("container");
 window.onscroll = function() {
     var middleOfWindow = window.innerHeight/4 + window.scrollY;
     containerEl.style.transformOriginY = middleOfWindow + "px";
-    console.log(middleOfWindow);
+    // console.log(middleOfWindow);
+}
+
+function moveToTop() {
+
+    var interval = setInterval(function() {
+        window.scrollTo(0, window.scrollY - scrollY*.03);
+        console.log("1");
+        if (window.scrollY == 0) {
+            clearInterval(interval);
+            selectProject();
+        }
+    },1);
+  
+}
+
+function selectProject() {
+    containerEl.style.transform = "none";
+    containerEl.style.top = "0";
+    containerEl.style.transition = "all .5s";
+
+    containerEl.removeChild(project2);
+    containerEl.removeChild(project3);
+    containerEl.removeChild(project4);
+    containerEl.removeChild(project5);
+    containerEl.removeChild(project6);
+    containerEl.removeChild(project7);
 }
