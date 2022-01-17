82e02d329443d6ac82253813c5bbbc8d245bcde8
antieatingactivist <antieatingactivist@gmail.com>
2022-01-17 00:56:56 -0800

    added info section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,86 @@
 // console.log("hello");
 containerEl = document.getElementById("container");
-project1 = document.getElementById("p1");
-project2 = document.getElementById("p2");
-project3 = document.getElementById("p3");
-project4 = document.getElementById("p4");
-project5 = document.getElementById("p5");
-project6 = document.getElementById("p6");
-project7 = document.getElementById("p7");
+infoEl = document.getElementById("info");
 
 
 
 
 
-// console.log(middleOfWindow);
-// containerEl.style.transformOriginY = middleOfWindow;
 
 
-window.onscroll = function() {
+containerEl.addEventListener('click', function(e) {
+    if (e.target.classList.contains("clickable")) {
+        console.log(e.target.id);
+        scrollToTop(e.target.id);
+    }
+    
+});
+
+
+
+window.onscroll = adjustPerspective;
+adjustPerspective();
+
+function adjustPerspective() {
     var middleOfWindow = window.innerHeight/4 + window.scrollY;
     containerEl.style.transformOriginY = middleOfWindow + "px";
     // console.log(middleOfWindow);
 }
 
-function moveToTop() {
-
+function scrollToTop(id) {
+    fadeNotClicked(id);
     var interval = setInterval(function() {
         window.scrollTo(0, window.scrollY - scrollY*.03);
         console.log("1");
         if (window.scrollY == 0) {
             clearInterval(interval);
-            selectProject();
+            moveProjectToHeader(id);
+            
+
         }
     },1);
   
 }
+function fadeNotClicked(id) {
+   
+    for (i of containerEl.children) {
+        if (i.id == id) {
+            i.removeAttribute("class");
+            containerEl.prepend(i);
+        }
+    
+    }
+    for (i of containerEl.children) {
 
-function selectProject() {
+        // console.log(i);
+        if (i.id != id) {
+            
+            i.style.color = "#00000000";
+            
+        }
+       
+        
+
+    }
+}
+
+function moveProjectToHeader(id) {
     containerEl.style.transform = "translate(0%, 0vh) perspective(0px) rotateY(0deg)";
     containerEl.style.top = "0";
     containerEl.style.width = "100%";
     containerEl.style.transitionDuration = ".5s";
+    infoEl.style.display = "block"; 
+    
+    setTimeout(function() {
 
-    containerEl.removeChild(project2);
-    containerEl.removeChild(project3);
-    containerEl.removeChild(project4);
-    containerEl.removeChild(project5);
-    containerEl.removeChild(project6);
-    containerEl.removeChild(project7);
+        while (containerEl.children.length > 1) {
+
+            containerEl.removeChild(containerEl.lastChild);
+        }
+        // infoEl.style.display = "block";  
+        infoEl.style.backgroundColor = "#ffffff22";
+    },500);
+    
+
+
 }
